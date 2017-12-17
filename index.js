c261b2f5bb990525aee57f17a86f6e6a7d867ac8
Rakesh M Pujary <rakeshmpujary@yahoo.com>
2017-12-17 13:45:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+var uppercase = "HELLO!";
+ 
+uppercase.toUpperCase() === uppercase;
+ 
+var lowercase = 'hello!';
+ 
+lowercase.toLowerCase() === lowercase;
+ 
+var mixedCase = 'Hi there!';
+ 
+mixedCase.toLowerCase() === mixedCase; 
+ 
+mixedCase.toUpperCase() === mixedCase;
